98e16457efecffe6874b19f8bbac1f9e86fc973c
SUMANTH1010 <62061486+SUMANTH1010@users.noreply.github.com>
2022-05-08 17:02:55 +0530

    Delete SpeakGoodBye.js

--- SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var byeSpeaker = new Object();
-    var greet = "Good Bye";
-    byeSpeaker.speak = function speak(person) {
-        console.log(greet + " " + person);
-    };
-    window.byeSpeaker = byeSpeaker;
-})(window);
